27e7efaf5aa2d47ead72258d62c5e1be10f11252
Karol Zając <karolzajac.0407@gmail.com>
2024-01-19 20:29:53 +0100

    fix: card-value as string

--- src/card-lib/types/CardValue.ts
+++ src/card-lib/types/CardValue.ts
@@ -1,16 +1,18 @@
 enum Value{
-    Ace = 1,
-    Two = 2,
-    Three = 3,
-    Four = 4,
-    Five = 5, 
-    Six = 6,
-    Seven = 7,
-    Eight = 8,
-    Nine = 9,
-    Ten = 10, 
-    Jack = 11,
-    Queen = 12,
-    King = 13,
+    Two = "2",
+    Three = "3",
+    Four = "4",
+    Five = "5", 
+    Six = "6",
+    Seven = "7",
+    Eight = "8",
+    Nine = "9",
+    Ten = "10", 
+    Jack = "J",
+    Queen = "Q",
+    King = "K",
+    Ace = "A"
 }
+
+
 export default Value;
