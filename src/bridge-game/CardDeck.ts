2be715fc9660b6c9603c8f68b8bafe6b1fda1258
Karol Zając <karolzajac.0407@gmail.com>
2024-01-19 21:10:59 +0100

    feat: card handling functions

---
+++ src/bridge-game/CardDeck.ts
@@ -0,0 +1,47 @@
+import CardSuit from "../card-lib/types/CardSuit";
+import CardValue from "../card-lib/types/CardValue";
+
+type CardType = { value: CardValue; suit: CardSuit };
+
+export function getCardValue(card: CardType): number {
+  return (
+    Object.values(CardSuit).indexOf(card.suit) * 13 +
+    Object.keys(CardValue).indexOf(card.value)
+  );
+}
+
+export function createCardDeck(): Array<CardType> {
+  const cardDeck: Array<CardType> = [];
+  for (const suit in CardSuit) {
+    for (const value in CardValue) {
+      cardDeck.push({ value: value as CardValue, suit: suit as CardSuit });
+    }
+  }
+  return cardDeck;
+}
+
+export function shuffleCardDeck(cards: Array<CardType>): Array<CardType> {
+  const shuffledCards = [...cards];
+  for (let i = shuffledCards.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [shuffledCards[i], shuffledCards[j]] = [shuffledCards[j], shuffledCards[i]];
+  }
+  return shuffledCards;
+}
+
+export function distributeCardsIntoPlayers(
+  cards: Array<CardType>
+): Array<Array<CardType>> {
+  const playersCards = [];
+  for (let i = 0; i < cards.length; i += 13) {
+    const part = cards.slice(i, i + 13);
+    playersCards.push(part);
+  }
+  return playersCards;
+}
+
+export function orderCards(cards: Array<CardType>): Array<CardType> {
+  return cards.sort((a, b) => {
+    return getCardValue(a) - getCardValue(b);
+  });
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import { Card } from './card-lib/Card'
-import Suit from './card-lib/types/CardSuit'
-import Value from './card-lib/types/CardValue'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import Test from "./Game";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <div className='flex [&>*]:h-[5rem]'>
-      essa
-    <Card   suit={Suit.Clubs}   value={Value.Ace} />
-      <img src='/src/assets/clubs-svgrepo-com.svg'/>
-      <img src='/src/assets/diamonds-svgrepo-com.svg'/>
-      <img src='/src/assets/spades-svgrepo-com.svg'/>
-      <img src='/src/assets/heart-svgrepo-com.svg'/>
-    </div>
-  </React.StrictMode>,
-)
+    <Test />
+  </React.StrictMode>
+);
